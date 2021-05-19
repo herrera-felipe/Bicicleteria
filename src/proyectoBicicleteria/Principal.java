d9230485bd5c2add3261e765264025e2dd43a1f0
Felipe Herrera <herrerafelipe.093@gmail.com>
2021-05-19 13:58:28 -0300

    Se modifico Principal.java

--- src/proyectoBicicleteria/Principal.java
+++ src/proyectoBicicleteria/Principal.java
@@ -9,18 +9,23 @@ public class Principal {
     
     public static void main(String[] args) {
         
-        Bicicleteria nuevaBicicleteria = new Bicicleteria();
+        Bicicleteria sedeMendoza = new Bicicleteria();
         
         Bicicleta bicicleta_1 = new Bicicleta("1234", "Mountain Bike", 2020, 0);
         bicicleta_1.setPrecio(9999.99f);
         
         Bicicleta bicicleta_2 = new Bicicleta("8520", "Scott", 2019, 20500);
         
-        nuevaBicicleteria.addBicicleta(bicicleta_1);
-        nuevaBicicleteria.addBicicleta(bicicleta_2);
+        sedeMendoza.addBicicleta(bicicleta_1);
+        sedeMendoza.addBicicleta(bicicleta_2);
         
-        System.out.println(nuevaBicicleteria.buscarBicicleta("8520"));
+        System.out.println("Precio total de las bicicletas: $" + sedeMendoza.precioTotalBicicletas());
+        
+        // Llamada al metodo buscar bicicleta por # serie
+        System.out.println(sedeMendoza.buscarBicicleta("8520"));
               
-        nuevaBicicleteria.venderBicicleta(bicicleta_1);
+        sedeMendoza.venderBicicleta(bicicleta_1);
+        
+        
     }
 }
